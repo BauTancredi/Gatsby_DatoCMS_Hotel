357608a8acdd0a9a05faebb0369d1e503fcb9677
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-25 17:15:32 -0300

    Adding styles to header and navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,38 @@
 import React from "react"
+import styled from "@emotion/styled"
+
+import Nav from "./nav"
+
+const Head = styled.header`
+  background-color: #333;
+  padding: 1rem;
+`
+const HeaderContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`
+
+const H1 = styled.h1`
+  color: #fff;
+  text-align: center;
+`
 
 const Header = () => {
-  return <h1>Header</h1>
+  return (
+    <Head>
+      <HeaderContainer>
+        <H1>Gastby Hotel</H1>
+
+        <Nav />
+      </HeaderContainer>
+    </Head>
+  )
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
 import { Global, css } from "@emotion/core"
-import { styled } from "@emotion/styled"
 
 import Header from "./header"
 
@@ -45,7 +44,10 @@ const Layout = props => {
       />
       <Helmet>
         <title>Gastby Hotel</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          rel="stylesheet"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
