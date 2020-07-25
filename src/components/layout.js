4dbaf9cfb78d95bd940ebb701a60cb2ed30b8539
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-25 15:48:28 -0300

    Add React Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,19 @@
 import React from "react"
+import Helmet from "react-helmet"
 
 import Header from "./header"
 
 const Layout = props => {
   return (
     <>
+      <Helmet>
+        <title>Gastby Hotel</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header />
       {props.children}
     </>
