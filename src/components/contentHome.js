c2e1609fb339dd0a23702035aacb1070a6ff116d
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-26 13:40:19 -0300

    css to home page

--- src/components/contentHome.js
+++ src/components/contentHome.js
@@ -1,5 +1,30 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
+import styled from "@emotion/styled"
+
+const H2 = styled.h2`
+  text-align: center;
+  font-size: 4rem;
+  margin-top: 4rem;
+`
+
+const TextHome = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+
+  p {
+    line-height: 2;
+  }
+`
 
 const ContentHome = () => {
   const info = useStaticQuery(graphql`
@@ -22,12 +47,12 @@ const ContentHome = () => {
   const { title, content, image } = info.allDatoCmsPage.nodes[0]
   return (
     <>
-      <h2>{title}</h2>
+      <H2>{title}</H2>
 
-      <div>
+      <TextHome>
         <p>{content}</p>
-        <img src={image.fluid.src} />
-      </div>
+        <Image fluid={image.fluid} />
+      </TextHome>
     </>
   )
 }
