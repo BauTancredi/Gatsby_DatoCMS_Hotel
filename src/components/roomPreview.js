8baad4779739603d562f0612191a43abdea766db
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-26 14:01:40 -0300

    roomPreview component

---
+++ src/components/roomPreview.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Image from "gatsby-image"
+
+const RoomPreview = ({ room }) => {
+  const { title, content, slug, image } = room
+  return (
+    <div>
+      <Image fluid={image.fluid} />
+      <div>
+        <h3>{title}</h3>
+        <p>{content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default RoomPreview
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "@emotion/styled"
+
 import ImageHotel from "../components/imageHotel"
 import ContentHome from "../components/contentHome"
+import RoomPreview from "../components/roomPreview"
 
-const IndexPage = () => (
-  <Layout>
-    <ImageHotel />
-    <ContentHome />
-  </Layout>
-)
+import UseRooms from "../hooks/use-rooms"
+
+const H2 = styled.h2`
+  text-align: center;
+  font-size: 3rem;
+  margin-top: 5rem;
+`
+
+const IndexPage = () => {
+  const rooms = UseRooms()
+
+  return (
+    <Layout>
+      <ImageHotel />
+      <ContentHome />
+
+      <H2>Our Rooms</H2>
+      <ul>
+        {rooms.map(room => (
+          <RoomPreview key={room.id} room={room} />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
