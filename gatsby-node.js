9b4b4a6ee110d20f7efceead2d01dff386b78792
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-26 14:48:00 -0300

    Generating dinamic pages programatically for rooms

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allDatoCmsBedroom {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
 
-// You can delete this file if you're not using it
+  if (result.errors) {
+    reporter.panic("There are no results", result.errors)
+  }
+
+  const rooms = result.data.allDatoCmsBedroom.nodes
+
+  rooms.forEach(room => {
+    actions.createPage({
+      path: room.slug,
+      component: require.resolve("./src/components/rooms.js"),
+      context: {
+        slug: room.slug,
+      },
+    })
+  })
+}
---
+++ src/components/rooms.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const RoomTemplate = () => {
+  return <h1>Room Template</h1>
+}
+
+export default RoomTemplate
